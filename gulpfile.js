212c8e0b975e2721bbe0e532dc613e50c9bc7b19
Anitha <anithavallam@yahoo.com>
2015-02-09 13:56:40 -0500

    Added the watch

--- gulpfile.js
+++ gulpfile.js
@@ -47,5 +47,10 @@ gulp.src(sassSources)
 .pipe(gulp.dest('builds/development/css'))
 });
 
+gulp.task('watch', function() {
+gulp.watch(coffeeSources, ['coffee']);
+gulp.watch(jsSources, ['js']);
+gulp.watch('components/sass/*.scss', ['compass']);
+});
 
 gulp.task('default',['coffee','js','compass']);
